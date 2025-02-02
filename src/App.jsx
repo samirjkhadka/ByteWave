ecfce6fe7afc31d1f668b7a4b058d7e7979c4416
samirjkhadka <findsamirkhadka@gmail.com>
2025-02-02 13:12:49 +0545

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,23 @@ import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
 function App() {
   return (
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Header />
-      <Hero />
-      <Benefits />
-      <Collaboration />
-      <Services />
-      <Pricing />
-      <Roadmap />
-      <Footer />
-    </div>
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
+      <ButtonGradient />
+    </>
   );
 }
 
