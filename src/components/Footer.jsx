95798a18dd42d8a7e943096a8ee1b581759b2000
samirjkhadka <findsamirkhadka@gmail.com>
2025-02-02 10:59:47 +0545

    Footer section complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,29 @@
-
+import Section from "./Section";
+import { socials } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Section cross className="!px-0 !py-10">
+      <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
+        <p className="caption text-n-4 lg:block">
+          © {new Date().getFullYear()} Byte Wave. All rights reserved
+        </p>
+        <ul className="flex gap-5 flex-wrap">
+          {socials.map((item) => (
+            <a href="" key={item} className="">
+              <img
+                src={item.iconUrl}
+                alt={item.title}
+                width={16}
+                height={16}
+                className=""
+              />
+            </a>
+          ))}
+        </ul>
+      </div>
+    </Section>
+  );
+};
 
-export default Footer
+export default Footer;
