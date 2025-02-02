7e5d95780df073ace4604f1eaef0e4b2eab2ef2a
samirjkhadka <findsamirkhadka@gmail.com>
2025-02-02 12:28:14 +0545

    Pricing component complete

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,9 +1,51 @@
-
+import Section from "./Section";
+import Heading from "./Heading";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/Pricing";
+import { smallSphere, stars } from "../assets";
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
-  )
-}
+    <Section className="overflow-hidden" id="pricing">
+      <div className="container relative z-2">
+        <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
+          <img
+            src={smallSphere}
+            width={255}
+            height={255}
+            alt="Sphere"
+            className="relative z-1"
+          />
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[60rem] pointer-events-none">
+            <img
+              src={stars}
+              width={950}
+              height={400}
+              alt="Stars"
+              className="w-full"
+            />
+          </div>
+        </div>
+        <Heading
+          tag="Get started with Brainwave"
+          title="Pay once, use forever"
+        />
+        <div className="">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+        <div className="flex justify-center mt-10">
+          <a
+            href="/pricing"
+            className="text-xs font-code font-bold tracking-wider uppercase border-b"
+          >
+            See the full details
+          </a>
+        </div>
+      </div>
+    </Section>
+  );
+};
 
-export default Pricing
+export default Pricing;
--- src/components/design/Pricing.jsx
+++ src/components/design/Pricing.jsx
@@ -1,9 +1,19 @@
-
-
-const Pricing = () => {
+import { lines } from "../../assets";
+export const LeftLine = () => {
   return (
-    <div>Pricing</div>
-  )
-}
-
-export default Pricing
+    <div className="hidden lg:block absolute top-1/2 right-full w-[92.5rem] h-[11.0625rem] -translate-y-1/2 pointer-events-none">
+      <img
+        src={lines}
+        alt="Lines"
+        width={1480}
+        height={177}
+        className="w-full"
+      />
+    </div>
+  );
+};
+export const RightLine = () => {
+  <div className="hidden lg:block absolute top-1/2 left-full w-[92.5rem] h-[11.0625rem] -translate-y-1/2 -scale-x-100 pointer-events-none">
+    <img src={lines} alt="Lines" height={177} width={1480} className="w-full" />
+  </div>;
+};
